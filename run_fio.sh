e2a154eea8aaaddd82af3590c2b40e98fc68c8b1
Ludovic Francois <ludo@msolution.io>
2014-12-10 05:02:30 -0500

    updated README file with a set of instructions to run sls-fio-test

--- run_fio.sh
+++ run_fio.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+command -v fio >/dev/null 2>&1 || { echo >&2 "I require fio but it's not installed.  Aborting."; exit 1; }
 [[ -d /tmp/fio-testing/data ]] || mkdir -p /tmp/fio-testing/data
 for i in *fio
 do
