c2d6ea8c026e1f1f83fc0c4ed61a048aaa5d391e
Ludovic Francois <ludo@msolution.io>
2014-12-11 19:58:24 -0500

    made an exception for file *.fio and not only *fio files

--- run_fio.sh
+++ run_fio.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 command -v fio >/dev/null 2>&1 || { echo >&2 "I require fio but it's not installed.  Aborting."; exit 1; }
 [[ -d /tmp/fio-testing/data ]] || mkdir -p /tmp/fio-testing/data
-for i in *fio
+for i in *.fio
 do
  printf "%-30s ===\t\t" $i
  fio $i | grep aggr | awk '{ print $3 }' | sed 's/aggrb=//' | tr ',' ' '
